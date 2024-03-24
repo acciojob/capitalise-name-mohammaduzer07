fd60077fe7f7ef3fba349fabf07ca6e36a196316
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-03-24 16:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let input = document.getElementById('#fname')
+
+input.addEventListner("blur", CapitalizeName);
+
+function CapitalizeName() {
+	let capName = input.value;
+	input.value = capName.toUpperCase();
+}
